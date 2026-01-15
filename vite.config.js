ebbb06a6b7af4ca4307cd7307d593a427b0ad949
Simao <devpages4@gmail.com>
2026-01-15 21:13:29 +0000

    fix: Configure PWA to cleanup outdated caches - fixes Chrome login loop

--- vite.config.js
+++ vite.config.js
@@ -43,6 +43,9 @@ export default defineConfig({
         screenshots: []
       },
       workbox: {
+        cleanupOutdatedCaches: true,
+        skipWaiting: true,
+        clientsClaim: true,
         globPatterns: ['**/*.{js,css,html,ico,png,svg,woff2}'],
         runtimeCaching: [
           {
